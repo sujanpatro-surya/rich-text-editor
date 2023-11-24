d97fc2fb749ce1d59c5f98fe5276c4030b475601
Sujan <sujan.kp@surya-digital.com>
2023-11-24 16:58:09 +0530

    add basic

--- src/editors/mdx-editor.tsx
+++ src/editors/mdx-editor.tsx
@@ -1,5 +1,7 @@
-const MDXEditor = (): React.ReactElement => {
-  return <></>;
+import "@mdxeditor/editor/style.css";
+import { MDXEditor, headingsPlugin } from "@mdxeditor/editor";
+const SomeThing = (): React.ReactElement => {
+  return <MDXEditor markdown={"# Hello World"} plugins={[headingsPlugin()]} />;
 };
 
-export default MDXEditor;
+export default SomeThing;
