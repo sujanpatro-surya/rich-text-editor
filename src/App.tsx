ada779cf46dff26cf6aee910b3bb9a1899c879eb
Sujan <sujan.kp@surya-digital.com>
2023-11-28 16:35:24 +0530

    Change default master settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeConfig } from "@surya-digital/leo-reactjs-material-ui";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { darkColorPalette } from "./theme/color-palette/darkColorPalette";
+import { lightColorPalette } from "./theme/color-palette/lightColorPalette";
+import { typographyStyles } from "./theme/typography/typographyStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeConfig
+      theme="light"
+      baseColorTokens={[
+        { name: "light", colorTokens: lightColorPalette },
+        { name: "dark", colorTokens: darkColorPalette },
+      ]}
+      typography={typographyStyles}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeConfig>
   );
 }
 
